37b1071543928082de3c7803372abb792064d7a0
mgalushka <mgalushka@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2010-12-29 18:59:41 +0000

    more cleanup

--- trunk/src/com/db/portforward/config/ConfigurationException.java
+++ trunk/src/com/db/portforward/config/ConfigurationException.java
@@ -10,6 +10,8 @@ package com.db.portforward.config;
  * @author mgalushka
  */
 public class ConfigurationException extends Exception{
+    
+    private static final long serialVersionUID = 5165608627375637327L;
 
     ConfigurationException(Exception ex) {
         super("Configuration exception");
