0286aad856b84087f21bbc2beff3adf5f4db6801
mgalushka <mgalushka@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2010-12-30 12:41:47 +0000

    Improvements to run on Java 1.5

--- trunk/src/com/db/portforward/utils/PathUtils.java
+++ trunk/src/com/db/portforward/utils/PathUtils.java
@@ -1,7 +1,9 @@
 package com.db.portforward.utils;
 
 import java.io.File;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  *
@@ -15,7 +17,7 @@ public class PathUtils {
     
     public static File getFile(String path) throws MalformedURLException{
         URL url = new URL("file://" + path);
-        File resultFile;
+        File resultFile = null;
         try {
             resultFile = new File(url.toURI());
         } catch(URISyntaxException e) {
