4f03edd61570d24fd64697bc191efedc1eb59efe
mgalushka <mgalushka@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2011-01-10 21:33:44 +0000

    Application exception cleaned-up

--- trunk/src/com/db/portforward/ApplicationException.java
+++ trunk/src/com/db/portforward/ApplicationException.java
@@ -12,14 +12,6 @@ public class ApplicationException extends Exception{
     }
 
     public ApplicationException(String message) {
-        super(message);
-    }
-
-    public ApplicationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ApplicationException(Throwable cause) {
-        super(cause);
+        super("Application exception: " + message);
     }
 }
