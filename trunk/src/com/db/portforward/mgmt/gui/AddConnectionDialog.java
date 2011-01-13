37272603de951793bd58253b8020638149bdaa0e
mgalushka <mgalushka@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2011-01-13 20:03:40 +0000

    GridBagLayout layout for Add Connection dialog

--- trunk/src/com/db/portforward/mgmt/gui/AddConnectionDialog.java
+++ trunk/src/com/db/portforward/mgmt/gui/AddConnectionDialog.java
@@ -12,18 +12,48 @@ public class AddConnectionDialog extends JDialog {
     public AddConnectionDialog(Frame owner) {
         super(owner, "Add Connection", true);
 
+        setResizable(false);
+
         //Handle window closing correctly.
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         JPanel contentPane = new JPanel();
+        contentPane.setLayout(new GridBagLayout());
 
-        contentPane.add(new JTextField(""));
-        contentPane.add(new JTextField(""));
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets( 2, 2, 2, 2 );
+        c.gridx = 0; // Column 0
+        c.gridy = 0; // Row 0
+        c.gridwidth = 2;
+        contentPane.add( new JLabel( "Port" ), c );
 
-        contentPane.add(new JButton("Add"));
-        contentPane.add(new JButton("Cancel"));
+        c.gridx = 2; // Column 2
+        c.gridwidth = 3;
+        contentPane.add( new JTextField(10), c );
 
-        setContentPane(contentPane);
+        c.gridx = 0; // Column 0
+        c.gridy = 1; // Row 1
+        c.gridwidth = 2;
+        contentPane.add( new JLabel( "URL" ), c );
+
+        c.gridx = 2; // Column 2
+        c.gridwidth = 3;
+        contentPane.add( new JTextField(10), c );
+
+
+        c.gridx = 1; // Column 1
+        c.gridy = 2; // Row 2
+        c.gridwidth = 2;
+        contentPane.add( new Button("Add"), c );
+
+
+        c.gridx = 3; // Column 1
+        c.gridwidth = 2;
+        contentPane.add( new Button("Cancel"), c );
+
         
+        setContentPane(contentPane);
+
     }
+
 }
