9b329e5a50e2c7fd9af273a563b811c5dd55a1fe
mgalushka <mgalushka@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2011-01-13 22:05:22 +0000

    connections dialog - add/cancel buttons

--- trunk/src/com/db/portforward/mgmt/gui/AddConnectionDialog.java
+++ trunk/src/com/db/portforward/mgmt/gui/AddConnectionDialog.java
@@ -1,13 +1,27 @@
 package com.db.portforward.mgmt.gui;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import com.db.portforward.config.PortForwardRecord;
 
 /**
  * @author Maxim Galushka
  * @since 11.01.2011
  */
-public class AddConnectionDialog extends JDialog {
+public class AddConnectionDialog extends JDialog implements ActionListener{
+
+    private static Log log = LogFactory.getLog(AddConnectionDialog.class);
+
+    private JTextField portTextField;
+    private JTextField urlTextField;
+
+    private PortForwardRecord record;
 
     public AddConnectionDialog(Frame owner) {
         super(owner, "Add Connection", true);
@@ -25,35 +39,56 @@ public class AddConnectionDialog extends JDialog {
         c.gridx = 0; // Column 0
         c.gridy = 0; // Row 0
         c.gridwidth = 2;
-        contentPane.add( new JLabel( "Port" ), c );
+        contentPane.add(new JLabel("Port"), c);
 
         c.gridx = 2; // Column 2
         c.gridwidth = 3;
-        contentPane.add( new JTextField(10), c );
+        portTextField = new JTextField(10);
+        contentPane.add(portTextField, c);
 
         c.gridx = 0; // Column 0
         c.gridy = 1; // Row 1
         c.gridwidth = 2;
-        contentPane.add( new JLabel( "URL" ), c );
+        contentPane.add(new JLabel("URL"), c);
 
         c.gridx = 2; // Column 2
         c.gridwidth = 3;
-        contentPane.add( new JTextField(10), c );
+        urlTextField = new JTextField(10);
+        contentPane.add(urlTextField, c);
 
 
         c.gridx = 1; // Column 1
         c.gridy = 2; // Row 2
         c.gridwidth = 2;
-        contentPane.add( new Button("Add"), c );
+        JButton add = new JButton("Add");
+        add.setActionCommand("Add");
+        add.addActionListener(this);
+        contentPane.add(add, c);
 
 
         c.gridx = 3; // Column 1
         c.gridwidth = 2;
-        contentPane.add( new Button("Cancel"), c );
-
+        JButton cancel = new JButton("Cancel");
+        cancel.setActionCommand("Close");
+        cancel.addActionListener(this);
+        contentPane.add(cancel, c);
         
         setContentPane(contentPane);
 
     }
 
+    public PortForwardRecord getRecord() {
+        return record;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Close")){
+            dispose();
+        }
+        if(e.getActionCommand().equals("Add")){
+            log.debug("Add action called");
+            dispose();
+        }
+    }
 }
