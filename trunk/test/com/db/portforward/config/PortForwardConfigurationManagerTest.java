0546cd8ed38a97f417754581375dffebf4aff7b4
mgalushka@gmail.com <mgalushka@gmail.com@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2011-08-18 08:27:46 +0000

    added test for new configuration manager

---
+++ trunk/test/com/db/portforward/config/PortForwardConfigurationManagerTest.java
@@ -0,0 +1,37 @@
+package com.db.portforward.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.cglib.core.CollectionUtils;
+
+/**
+ * <p></p>
+ *
+ * @author Maxim Galushka
+ * @since 18/08/2011
+ */
+public class PortForwardConfigurationManagerTest {
+
+    private static Log log = LogFactory.getLog(PortForwardConfigurationManagerTest.class);
+
+    private ConfigurationManager cm;
+
+    @Before
+    public void init(){
+    }
+
+    @After
+    public void clear(){
+    }
+
+    @Test
+    public void testGetConfiguration() throws Exception {
+        cm = PortForwardConfigurationManager.getConfigurationManager();
+        log.debug(cm.getConfiguration().toString());
+
+
+    }
+}
