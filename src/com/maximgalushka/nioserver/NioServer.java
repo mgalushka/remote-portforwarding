720f49d4b2c85f7f707d5b649cd928c2bed81978
mgalushka@gmail.com <mgalushka@gmail.com@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2012-06-21 18:35:02 +0000

    added some nio testing classes

---
+++ src/com/maximgalushka/nioserver/NioServer.java
@@ -0,0 +1,111 @@
+package com.maximgalushka.nioserver;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+
+/**
+ * <p></p>
+ *
+ * @author Maxim Galushka
+ * @since 21.06.12
+ */
+public class NioServer implements Runnable {
+    // The host:port combination to listen on
+    private InetAddress hostAddress;
+    private int port;
+
+    // The channel on which we'll accept connections
+    private ServerSocketChannel serverChannel;
+
+    // The selector we'll be monitoring
+    private Selector selector;
+
+    // The buffer into which we'll read data when it's available
+    private ByteBuffer readBuffer = ByteBuffer.allocate(8192);
+
+    public NioServer(InetAddress hostAddress, int port) throws IOException {
+        this.hostAddress = hostAddress;
+        this.port = port;
+        this.selector = this.initSelector();
+    }
+
+    private Selector initSelector() throws IOException {
+        // Create a new selector
+        Selector socketSelector = SelectorProvider.provider().openSelector();
+
+        // Create a new non-blocking server socket channel
+        this.serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+
+        // Bind the server socket to the specified address and port
+        InetSocketAddress isa = new InetSocketAddress(this.hostAddress, this.port);
+        serverChannel.socket().bind(isa);
+
+        // Register the server socket channel, indicating an interest in
+        // accepting new connections
+        serverChannel.register(socketSelector, SelectionKey.OP_ACCEPT);
+
+        return socketSelector;
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Thread(new NioServer(null, 9090)).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                // Wait for an event one of the registered channels
+                this.selector.select();
+
+                // Iterate over the set of keys for which events are available
+                Iterator selectedKeys = this.selector.selectedKeys().iterator();
+                while (selectedKeys.hasNext()) {
+                    SelectionKey key = (SelectionKey) selectedKeys.next();
+                    selectedKeys.remove();
+
+                    if (!key.isValid()) {
+                        continue;
+                    }
+
+                    // Check what event is available and deal with it
+                    if (key.isAcceptable()) {
+                        this.accept(key);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void accept(SelectionKey key) throws IOException {
+        // For an accept to be pending the channel must be a server socket channel.
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+
+        // Accept the connection and make it non-blocking
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        Socket socket = socketChannel.socket();
+        socketChannel.configureBlocking(false);
+
+        // Register the new SocketChannel with our Selector, indicating
+        // we'd like to be notified when there's data waiting to be read
+        socketChannel.register(this.selector, SelectionKey.OP_READ);
+    }
+
+
+}
