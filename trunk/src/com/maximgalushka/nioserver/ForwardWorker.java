bea6d2faabb1fc34f857bac7e28b0d3d670a307b
mgalushka@gmail.com <mgalushka@gmail.com@1e971c00-118f-ca5a-bdb2-46f5dfe08765>
2012-06-22 09:14:35 +0000

    re-factored to using BlockingQueue.

--- trunk/src/com/maximgalushka/nioserver/ForwardWorker.java
+++ trunk/src/com/maximgalushka/nioserver/ForwardWorker.java
@@ -1,43 +1,38 @@
 package com.maximgalushka.nioserver;
 
-import java.io.IOException;
 import java.nio.channels.SocketChannel;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class ForwardWorker implements Runnable {
 
-    private List queue = new LinkedList();
+    private final BlockingQueue<ServerDataEvent> bqueue = new ArrayBlockingQueue<ServerDataEvent>(100);
 
     public void processData(NioServer server, SocketChannel socket, byte[] data, int count) {
         byte[] dataCopy = new byte[count];
+
+        // TODO: weak references here to make sure ...
         System.arraycopy(data, 0, dataCopy, 0, count);
-        synchronized(queue) {
-            queue.add(new ServerDataEvent(server, socket, dataCopy));
-            queue.notify();
+        try {
+            bqueue.put(new ServerDataEvent(server, socket, dataCopy));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void run() {
-        ServerDataEvent dataEvent;
-
+        //noinspection InfiniteLoopStatement
         while(true) {
-            // Wait for data to become available
-            synchronized(queue) {
-                while(queue.isEmpty()) {
-                    try {
-                        queue.wait();
-                    } catch (InterruptedException e) {
-                    }
-                }
-                dataEvent = (ServerDataEvent) queue.remove(0);
-            }
-
             try {
-                // Return to sender
-                dataEvent.server.send(dataEvent.socket, dataEvent.data);
-            } catch (IOException e) {
+                // Wait for data to become available
+                ServerDataEvent dataEvent = bqueue.take();
+                if(dataEvent != null){
+                    // TODO: do forwarding logic
+                    // Return to sender
+                    // dataEvent.server.send(dataEvent.socket, dataEvent.data);
+                }
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
